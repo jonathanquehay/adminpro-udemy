92a8ed6e7f8d79054764cce18e50047afc6113b8
JONATHAN MORENO <jonathanmoreno@MacBook-Pro-de-JONATHAN.local>
2018-01-15 09:15:22 -0600

    Primer commit

---
+++ src/app/shared/sidebar--spec/sidebar--spec.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar--spec',
+  templateUrl: './sidebar--spec.component.html',
+  styles: []
+})
+export class SidebarSpecComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
